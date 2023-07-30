64f69d610f08b1fb1ce80d7ce6d813c962065ed3
AlexKovalchuk <kovalchuk111@gmail.com>
2023-07-31 01:26:33 +0300

    HW #32  vers. from classwork

--- src/telran/io/InputOutputTest.java
+++ src/telran/io/InputOutputTest.java
@@ -2,59 +2,67 @@ package telran.io;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.FilePermission;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InputOutputTest {
 
+	private static final Path FILE_PATH = Path.of("/Users/ASUS/Documents/abc.txt");
+	private static final String NOT_EXIST = "doesn't exist";
+	private static final String NO_DIRECTORY = "no directory";
+
 	@BeforeEach
 	void setUp() throws Exception {
+		Files.createFile(FILE_PATH);
+	}
+	@AfterEach
+	void tearDown() throws Exception {
+		Files.delete(FILE_PATH);
 	}
 
-		@Test
-		void pathTest() {
-		Path pathParent = Path.of("../.."); 
-		System.out.println(pathParent.toAbsolutePath().normalize().getName(3));
-		System.out.println(pathParent.toAbsolutePath());
-		System.out.println(pathParent.toAbsolutePath().normalize());
-		System.out.println(pathParent.toAbsolutePath().normalize().getName(0));
-		System.out.println(pathParent.toAbsolutePath().normalize().getFileName());
-		System.out.println(pathParent.toAbsolutePath().normalize().getNameCount());
-		
-	}
-	
+
 	@Test
-	void displayDirContentTest() {
-		displayDirContent(Path.of("../.."),  3);
-		}
-
-	private void displayDirContent(Path path, int depth) {
-		
-		try {
-			if(!Files.isDirectory(path)) {
-        		throw new IllegalArgumentException(path.toString() + " is not directory");
-        	}
-			Files.walk(path, depth)
-			.filter(n -> Files.isReadable(n))
-			.forEach(n -> {
-				int offset = n.toAbsolutePath().normalize().getNameCount();
-				int offsetStart = path.toAbsolutePath().normalize().getNameCount();
-				String dirOrFile = Files.isDirectory(n) ? "dir" : "file"; 
-				System.out.printf("%s%s - %s\n", " ".repeat((offset - offsetStart) * 3), 
-						n.toAbsolutePath()
-						.normalize().getFileName(), dirOrFile);
-				});
-		} 
-
-		catch (IOException e) {
-			System.out.println(e.toString());
-		}
-		catch(IllegalArgumentException ex) {
-        	System.out.println(ex.toString());
-        }
-	}
+	void pathTest() {
+	Path pathParent = Path.of("../.."); 
+	System.out.println(pathParent.toAbsolutePath().normalize().getName(3));
+	System.out.println(pathParent.toAbsolutePath());
+	System.out.println(pathParent.toAbsolutePath().normalize());
+	System.out.println(pathParent.toAbsolutePath().normalize().getName(0));
+	System.out.println(pathParent.toAbsolutePath().normalize().getFileName());
+	System.out.println(pathParent.toAbsolutePath().normalize().getNameCount());
 	
+}
+
+	@Test
+	void displayDirContentTest() throws IOException {
+		displayDirContent(Path.of("../.."), 3);
+		assertThrowsExactly(IllegalArgumentException.class,
+				()->displayDirContent(Path.of("/Users/ASUS/Doc"), 1), NOT_EXIST);
+		
+		assertThrowsExactly(IllegalArgumentException.class,
+				()->displayDirContent(FILE_PATH, 1), NO_DIRECTORY);
+		
+	}
+
+	private void displayDirContent(Path dirPath, int maxDepth) throws IOException {
+		if(!Files.exists(dirPath)) {
+			throw new IllegalArgumentException("doesn't exist");
+		}
+		if (!Files.isDirectory(dirPath)) {
+			throw new IllegalArgumentException("no directory");
+		}
+		dirPath = dirPath.toAbsolutePath().normalize();
+		
+		int count = dirPath.getNameCount();
+		Files.walk(dirPath, maxDepth)
+				.forEach(p -> System.out.printf("%s%s - %s\n", "  ".repeat(p.getNameCount() - count),
+						p.getName(p.getNameCount() - 1), Files.isDirectory(p) ? "dir" : "file"));
+
+	}
 }
